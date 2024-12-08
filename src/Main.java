1c24d8b2795994a564838641fb8a0a3555707a59
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2024-12-08 14:56:53 +1000

    Check Arrays2

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,58 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        int[] threeNumbers = new int[3];
-        threeNumbers[0] = 1;
-        threeNumbers[1] = 2;
-        threeNumbers[2] = 3;
-        double[] numbers = {1.57, 7.654, 9.986};
+        int[] payMonth = new int[5];
+        payMonth[0] = 22300;
+        payMonth[1] = 18241;
+        payMonth[2] = 78960;
+        payMonth[3] = 33431;
+        payMonth[4] = 148202;
+        int sum = 0;
+        int sum2 = 0;
+
+        for (int element : payMonth) {
+            sum = sum + element;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
         //Задача 2
-        for (int i = 0; i < threeNumbers.length; i++) {
-            if (i < threeNumbers.length - 1) {
-                System.out.print(threeNumbers[i] + ", ");
-            } else System.out.println(threeNumbers[i]);
+        int[] payWeak = {32441, 150112, 553111, 240760, 225877};
+        int maxPayWeak = 0;
+        int minPayWeak = payWeak[0];
 
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            if (i < numbers.length - 1) {
-                System.out.print(numbers[i] + ", ");
-            } else System.out.print(numbers[i]);
-        }
-
-        //Задача 3
-        System.out.println();
-        for (int i = 2; i >= 0; i--) {
-            if (i > 0) {
-                System.out.print(threeNumbers[i] + ", ");
-            } else System.out.print(threeNumbers[i]);
-
-        }
-        System.out.println();
-        for (int i = 2; i >= 0; i--) {
-            if (i > 0) {
-                System.out.print(numbers[i] + ", ");
-            } else System.out.print(numbers[i]);
-        }
-
-        //Задача 4
-        System.out.println();
-        for (int i = 0; i < threeNumbers.length; i++) {
-            if (threeNumbers[i] % 2 == 1) {
-                threeNumbers[i] += 1;
+        for (int s : payWeak) {
+            if (s < minPayWeak) {
+                minPayWeak = s;
             }
         }
-            System.out.println(Arrays.toString(threeNumbers));
+        System.out.println("Минимальная сумма трат за неделю составила " + minPayWeak + " рублей");
 
+        for (int i = 0; i < payWeak.length; i++) {
+            if (payWeak[i] > maxPayWeak) {
+                maxPayWeak = payWeak[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + maxPayWeak + " рублей");
+
+        //Задача 3
+        int[] payMiddle = {5461123, 235003, 24001, 125301, 800127};
+        double middle;
+
+        for (int element : payMiddle) {
+            sum2 = sum2 + element;
+        }
+        middle = sum2 / payMiddle.length;
+        System.out.println("Средняя сумма трат за месяц составила " + middle + " рублей");
+
+        //Задача 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char tmp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - i - 1];
+            reverseFullName[reverseFullName.length - i - 1] = tmp;
+        }
+        for (int i = 0; i<reverseFullName.length; i++){
+            System.out.print(reverseFullName [i]);
+        }
     }
 }
